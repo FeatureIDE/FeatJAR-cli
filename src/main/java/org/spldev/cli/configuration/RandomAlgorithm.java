83b73c418f17e66befe9e7a5eb019517acd6374e
Elias Kuiter <info@elias-kuiter.de>
2022-07-11 13:39:03 +0200

    Updated license headers

--- src/main/java/org/spldev/cli/configuration/RandomAlgorithm.java
+++ src/main/java/org/spldev/cli/configuration/RandomAlgorithm.java
@@ -1,23 +1,23 @@
 /* -----------------------------------------------------------------------------
- * Command Line Interface - Reference frontend for the library
- * Copyright (C) 2021  Elias Kuiter
+ * cli -  Command Line Interface
+ * Copyright (C) 2021 Elias Kuiter
  * 
- * This file is part of Command Line Interface.
+ * This file is part of cli.
  * 
- * Command Line Interface is free software: you can redistribute it and/or modify it
+ * cli is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
+ * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  * 
- * Command Line Interface is distributed in the hope that it will be useful,
+ * cli is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Command Line Interface.  If not, see <https://www.gnu.org/licenses/>.
+ * along with cli. If not, see <https://www.gnu.org/licenses/>.
  * 
- * See <https://github.com/skrieter/cli> for further information.
+ * See <https://github.com/FeatJAR/cli> for further information.
  * -----------------------------------------------------------------------------
  */
 package org.spldev.cli.configuration;
